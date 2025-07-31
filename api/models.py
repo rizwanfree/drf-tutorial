f5eb77fa1add5c22a545bfca623a33484d048244
rizwanfree <121812303+rizwanfree@users.noreply.github.com>
2025-07-31 18:47:25 +0500

    models

---
+++ api/models.py
@@ -0,0 +1,44 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    @property
+    def in_stock(self):
+        return self.stock > 0
+    
+    def __str__(self):
+        return self.name
+    
+
+class Order(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = 'Pending'
+        CONFIRMED = 'Confirmed'
+        CANCEL = 'Cancel'
+
+    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=10, 
+        choices=StatusChoices.choices, 
+        default=StatusChoices.PENDING
+    )
+
+    def __str__(self):
+        return f"Order {self.order_id} by {self.user.username}"
+
